1befb2958ed2415fe6045349ba434efde7cde5e6
itsuki <itsuki.enjyoy@gmail.com>
2024-07-15 13:20:58 +0900

    Initial Commit

---
+++ HelloKeyboardDemo/HelloKeyboardDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloKeyboardDemoApp.swift
+//  HelloKeyboardDemo
+//
+//  Created by Itsuki on 2024/07/15.
+//
+
+import SwiftUI
+
+@main
+struct HelloKeyboardDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
